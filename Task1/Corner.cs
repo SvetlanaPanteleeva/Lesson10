77d20d657ca96da16de2ab295a4b0c9895a5f078
Светлана Пантелеева <panteleevas@gmail.com>
2023-02-02 20:33:32 +0300

    Добавьте файлы проекта.

---
+++ Task1/Corner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Corner
+    {
+        int gradus;
+        int minute;
+        int second;
+        public bool isCorrect = true;
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    gradus = value % 360;
+
+            }
+        }
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Gradus += value / 60;
+                    minute = value % 60;
+                }
+            }
+        }
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Minute += value / 60;
+                    second = value % 60;
+                }
+            }
+        }
+        public Corner(int gradus, int minute, int second)
+        {
+            this.Gradus = gradus;
+            this.Minute = minute;
+            this.Second = second;
+        }
+        public double ToRadians()
+        {
+            return Math.PI / 180 * (gradus + (minute + second / 60) / 60);
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradus_ = Convert.ToInt32(Console.ReadLine());
+            int minute_ = Convert.ToInt32(Console.ReadLine());
+            int second_ = Convert.ToInt32(Console.ReadLine());
+            Corner corner = new Corner(gradus_, minute_, second_);
+
+           double totalRadians = corner.ToRadians();
+            Console.WriteLine(totalRadians);
+            Console.ReadKey();
+        }
+    }
+}
